41160d0124987049be1cc3f3cad577350951d009
hahazss <45086418+hahazss@users.noreply.github.com>
2025-02-17 20:31:37 +0800

    Update process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -76,7 +76,7 @@ procs = [
   PythonProcess("dmonitoringmodeld", "selfdrive.modeld.dmonitoringmodeld", driverview, enabled=(not PC or WEBCAM)),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
-  NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
+  #NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
   NativeProcess("modeld", "selfdrive/modeld", ["./modeld"], only_onroad),
   #NativeProcess("mapsd", "selfdrive/navd", ["./mapsd"], model_use_nav),
   #PythonProcess("navmodeld", "selfdrive.modeld.navmodeld", model_use_nav),
@@ -117,7 +117,7 @@ procs = [
   # debug procs
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
   PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
-  #PythonProcess("webjoystick", "tools.bodyteleop.web", notcar),
+  PythonProcess("webjoystick", "tools.bodyteleop.web", notcar),
 
 
 ]
